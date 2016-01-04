03b5e3d36e0eea27902930d988ab2cd93c79bed7
Jason Le <le.kent.jason@gmail.com>
2016-01-04 11:56:05 -0500

    Fixed extra

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
-__author__ = 'owner'
+# </> with <3 by Jason, Patrick, Ricky and Justin
+import pygame as pi
+
+pi.init()
+
+# Display width
+dp_width = 800
+
+# Display height
+dp_height = 500
+
+window = pi.display.set_mode.GraphWin('Marsio', 500, 500)
+pi.display.set_caption('Marsio')
