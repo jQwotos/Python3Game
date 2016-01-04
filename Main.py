3a60d05031dff8b52de0b567c2ea85bd4812217a
Jason Le <le.kent.jason@gmail.com>
2016-01-04 12:12:09 -0500

    Added base code

--- Main.py
+++ Main.py
@@ -1,13 +1,45 @@
 # </> with <3 by Jason, Patrick, Ricky and Justin
 import pygame as pi
+from threading import Thread
 
 pi.init()
 
+# Get display info
+displayInfo = pi.display.Info()
 # Display width
-dp_width = 800
-
+dp_width = displayInfo.current_w
 # Display height
-dp_height = 500
+dp_height = displayInfo.current_h
 
-window = pi.display.set_mode.GraphWin('Marsio', 500, 500)
+# X, Y
+player = []
+
+# Set display
+screen = pi.display.set_mode((dp_width, dp_height))
 pi.display.set_caption('Marsio')
+
+# Holder player image
+playerImage = pi.image.load("img/projectile01.png")
+
+# Multithreaded quitting
+def Quitter():
+    while True:
+        for event in pi.event.get():
+            if event.type == pi.QUIT:
+                pi.quit()
+                exit()
+
+# Main program that draws
+def Main():
+    while True:
+        screen.fill((255, 255, 255))
+        screen.blit(playerImage)
+        pi.display.flip()
+
+
+# Initialize and start functions
+mainThread = Thread(target=Main)
+exitThread = Thread(target=Quitter)
+
+mainThread.start()
+exitThread.start()
