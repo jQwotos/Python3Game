fa9c552db4cbd713f79e60b412e814ca24197634
Jason Le <le.kent.jason@gmail.com>
2016-01-07 10:09:03 -0500

    Added loadMap.py

--- V2.py
+++ V2.py
@@ -1,20 +1,27 @@
-# </> with <3 by Jason Le
-# This is my implementation of dem cool classes
-
-import pygame, loadMap
+import loadMap,
+import pygame as pi
 from threading import Thread
 
-pygame.init()
+pi.init()
 
-displayInfo = pygame.display.Info()
+displayInfo = pi.display.Info()
 
 dp = {
     "width" : displayInfo.current_w,
     "height" : displayInfo.current_h
 }
 
-screen = pygame.display.set_mode((dp.get("width"), dp.get("height")))
-pygame.display.set_caption("Marsio")
+screen = pi.display.set_mode((dp.get("width"), dp.get("height")))
+pi.display.set_caption("Marsio")
 
 # Array is under map.level
 map = loadMap.load()
+
+def Main():
+    while True:
+        for event in pi.event.get():
+            if event.type == pi.QUIT():
+                pi.quit()
+                exit()
+            if event.type == pi.KEYDOWN:
+                pass
